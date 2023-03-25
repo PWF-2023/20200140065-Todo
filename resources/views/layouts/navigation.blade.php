28e316630c3691ffc7314c2929e4d414869de2f0
Muhammad Farid Gilang Nuswantoro <faridganteng20050@gmail.com>
2023-03-26 01:16:11 +0700

    env, mvc, route, relationship

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,18 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('todo.index')" :active="request()->routeIs('todo.index')">
+
+                        {{ __('Todo') }}
+
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('user.index')" :active="request()->routeIs('user.index')">
+
+                        {{ __('User') }}
+
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -70,6 +82,19 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
+            <x-nav-link :href="route('todo.index')" :active="request()->routeIs('todo.index')">
+
+                {{ __('Todo') }}
+
+            </x-nav-link>
+
+            <x-nav-link :href="route('user.index')" :active="request()->routeIs('user.index')">
+
+                {{ __('User') }}
+
+            </x-nav-link>
+
         </div>
 
         <!-- Responsive Settings Options -->
